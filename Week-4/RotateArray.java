c5519484216e56c776be9c747315a9557af735ca
Sumit <gurjarsumit366@gmail.com>
2026-09-20 14:56:39 +0530

    Add Rotate Array solution

---
+++ Week-4/RotateArray.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] rotateArray(int[] nums, int k) {
+
+        k = k % nums.length;
+
+        int[] newArray = new int[nums.length];
+
+        for (int i = 0; i < nums.length; i++) {
+            newArray[(i + k) % nums.length] = nums[i];
+        }
+
+        return newArray;
+    }
+}
